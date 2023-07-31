692d98dd0bb1580dc94596948548e4369c4dfd4f
Dan <sidato83@gmail.com>
2023-07-30 23:01:28 -0700

    Added AI to Player when not in camera focus.

--- Guardian's Havoc/Assets/Scripts/Player/PlayerOffFocusAI.cs
+++ Guardian's Havoc/Assets/Scripts/Player/PlayerOffFocusAI.cs
@@ -1,19 +1,60 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerOffFocusAI : MonoBehaviour
 {
+    [SerializeField] private NavMeshAgent playerAgent;
+    [SerializeField] private float destinationOffset = 0.2f;
+    [SerializeField] private Transform[] waypoints;
+
+    private Transform tf;
     private bool isSelected;
+    private bool isNavigating = false;
 
     void Start()
     {
-        
+        tf = GetComponent<Transform>();
     }
 
     void Update()
     {
         if (!isSelected)
         {
-            // AI Functionality Here.
+            playerAgent.enabled = true;
+            if (!isNavigating)
+            {
+                playerAgent.SetDestination(waypoints[0].position);
+                isNavigating = true;
+            }
+            else
+            {
+                NavigateWaypoints();
+            }
         }
+        else { playerAgent.enabled = false; }
+    }
+
+    private void NavigateWaypoints()
+    {
+        if (Vector3.Distance(tf.position, waypoints[0].position) < destinationOffset)
+        {
+            playerAgent.SetDestination(waypoints[1].position);
+        }
+
+        if (Vector3.Distance(tf.position, waypoints[1].position) < destinationOffset)
+        {
+            playerAgent.SetDestination(waypoints[0].position);
+        }
+    }
+
+    public void SetIsSelected(bool value)
+    {
+        isSelected = value;
+    }
+
+    public void SetIsNavigating(bool value)
+    {
+        isNavigating = value;
     }
 }
